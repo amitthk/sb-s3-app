b3eb57aabc1b2c414fc09d3893dd3227a9043ea1
amitthk <e.amitthakur@gmail.com>
2019-02-09 00:49:43 +0800

    added ui project

--- src/main/java/com/jvcdp/aws/s3/config/WebMvcConfig.java
+++ src/main/java/com/jvcdp/aws/s3/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.jvcdp.aws.s3.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,7 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter{
+public class WebMvcConfig extends WebMvcConfigurerAdapter  {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,6 +22,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
     @Override
